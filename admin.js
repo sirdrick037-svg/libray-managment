416e8a0c984672c0f8c19f54fdac04712600b1ec
sirdrick037 <sirdrick 037@gmail.com>
2026-08-06 09:45:41 +0300

    Fix access denial message and enhance user and book management functionality

--- admin.js
+++ admin.js
@@ -2,23 +2,17 @@ const currentUser = JSON.parse(localStorage.getItem("currentUser"));
 
 if (!currentUser || currentUser.role !== "admin") {
 
-    alert("Access denied!");
+    alert("Access Denied!");
 
     window.location.href = "index.html";
 
 }
 
-if (localStorage.getItem("loggedIn") !== "true") {
+let books = JSON.parse(localStorage.getItem("books")) || [];
 
-    window.location.href = "login.html";
+let users = JSON.parse(localStorage.getItem("users")) || [];
 
-}
 
-const books = JSON.parse(localStorage.getItem("books")) || [];
-
-const users = JSON.parse(localStorage.getItem("users")) || [];
-
-// Statistics
 
 document.getElementById("totalBooks").textContent = books.length;
 
@@ -28,18 +22,111 @@ books.filter(book => book.status === "Available").length;
 document.getElementById("borrowedBooks").textContent =
 books.filter(book => book.status === "Borrowed").length;
 
-// Users
+
+
 
 const userTable = document.getElementById("userTable");
 
-users.forEach(user => {
+users.forEach((user, index) => {
 
     const row = document.createElement("tr");
 
     row.innerHTML = `
-        <td class="border p-3">${user.username}</td>
+
+        <td class="border p-2">${user.username}</td>
+
+        <td class="border p-2">${user.role}</td>
+
+        <td class="border p-2">
+
+            <button
+            onclick="deleteUser(${index})"
+            class="bg-red-500 text-white px-3 py-1 rounded">
+
+            Delete
+
+            </button>
+
+        </td>
+
     `;
 
     userTable.appendChild(row);
+
+});
+
+
+
+
+const bookTable = document.getElementById("bookTable");
+
+books.forEach((book,index)=>{
+
+const row=document.createElement("tr");
+
+row.innerHTML=`
+
+<td class="border p-2">${book.bookId}</td>
+
+<td class="border p-2">${book.title}</td>
+
+<td class="border p-2">${book.author}</td>
+
+<td class="border p-2">${book.category}</td>
+
+<td class="border p-2">${book.status}</td>
+
+<td class="border p-2">
+
+<button
+onclick="deleteBook(${index})"
+class="bg-red-500 text-white px-3 py-1 rounded">
+
+Delete
+
+</button>
+
+</td>
+
+`;
+
+bookTable.appendChild(row);
+
+});
+
+
+function deleteUser(index){
+
+users.splice(index,1);
+
+localStorage.setItem("users",JSON.stringify(users));
+
+location.reload();
+
+}
+
+
+
+
+function deleteBook(index){
+
+books.splice(index,1);
+
+localStorage.setItem("books",JSON.stringify(books));
+
+location.reload();
+
+}
+
+
+
+
+document.getElementById("logoutBtn").addEventListener("click",()=>{
+
+localStorage.removeItem("loggedIn");
+
+localStorage.removeItem("currentUser");
+
+window.location.href="login.html";
 
 });
